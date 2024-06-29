078b8b2a989b98c9877be7ff7a81e85047eac656
Golemwulf <adams.timothy.w@gmail.com>
2024-06-29 14:31:32 -0500

    improved styling

--- src/Login.js
+++ src/Login.js
@@ -4,8 +4,11 @@ export default function Login(){
                <h3 className="text-center">Log In</h3>
                <br/>
                <input type="text" className="userName me-2" placeholder="Username"></input>
-               <input type="text" className="passWord me-2" placeholder="Password"></input>
-               <button id='submit'>submit</button>
+               <input type="password" className="passWord me-2" placeholder="Password"></input>
+               <br/><br/>
+               <a href="http://localhost:3000/">
+               <button id='submit' className=" btn btn-primary form-control">submit</button>
+               </a>
         </div>
     )
 
